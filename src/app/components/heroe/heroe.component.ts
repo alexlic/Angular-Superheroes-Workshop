90f2653f2ac5a4d8040aecb6af440c962633be0b
alexlic <alexlic6624@gmail.com>
2018-02-05 02:22:01 -0600

    Hero information component

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../models/heroe';
+import { HeroesService } from '../../services/heroes.service';
+
+
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe:Heroe;
+
+  constructor( private activatedRoute: ActivatedRoute,
+              private _heroesService: HeroesService) {
+
+    this.activatedRoute.params.subscribe(params=>{
+      //console.log(params.id);
+      //console.log(params['id']);
+      this.heroe = this._heroesService.getHeroe(params['id']);
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
